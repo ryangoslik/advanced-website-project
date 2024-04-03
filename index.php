120ec14004223ecfbfa11e516a8e78d616dbf7fe
unknown <konrad.bienkowski1@o2.pl>
2024-04-03 19:04:09 +0200

    123

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+session_start(); 
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -43,20 +46,63 @@
 
         <section class="description">
           <div class="opinia">
+            
           <div class="maintitle1">
             <h2>
               Napisz recenzję dla STAR WARS Jedi: Upadły zakon™
             </h2>
+            </div>
+            <div class="write">Opisz, co podobało ci się (lub nie) w tej grze i czy polecasz ją innym użytkownikom.</div>
+            <div class="komentarz">
+<form action="" method="post">
+    <label for="login">Nazwa:</label><br>
+    <input type="text" id="login" name="login" class="inputname"><br><br>
+    <label for="comment_text">Treść komentarza:</label><br>
+    <textarea id="comment_text" name="comment_text" rows="10" cols="100"></textarea><br><br>
+    <input type="submit" name="submit_comment" value="Dodaj komentarz" class="inputsend">
+</form>
+</div>
+</div>
+          
+          <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "kociteam";
 
-          </div>
-          <div class="write">Opisz, co podobało ci się (lub nie) w tej grze i czy polecasz ją innym użytkownikom.</div>
-        
-        <textarea name="" id="" cols="100" rows="10"></textarea>
-        <br>
-        <div class="pozycja">
-        <input class="input_opinie" type="submit" value="Prześlij opinię">
-      </div>
-    </div>
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if(isset($_POST['submit_comment'])) {
+    $comment_text = $_POST['comment_text'];
+    $login = $_POST['login'];
+
+    if(empty($login)) {
+        echo "Musisz podać swój nick, aby dodać komentarz.";
+    } else {
+        if(empty($comment_text)) {
+            echo "Komentarz nusi coś zawierać.";
+        } else {
+            $sql_check_comment = "SELECT * FROM Komentarze WHERE login = '$login' AND tresc = '$comment_text'";
+            $result_check_comment = $conn->query($sql_check_comment);
+            if($result_check_comment->num_rows > 0) {
+                echo "";
+            } else {
+                $sql_insert = "INSERT INTO Komentarze (login, tresc) VALUES ('$login', '$comment_text')";
+                if ($conn->query($sql_insert) === TRUE) {
+                    echo "Nowy komentarz został dodany.";
+                } else {
+                    echo "Błąd podczas dodawania komentarza: " . $conn->error;
+                }
+            }
+        }
+    }
+}
+?>
+          
     <hr>
     <div class="description_game">
       <h3>O TEJ GRZE</h3>
@@ -105,7 +151,22 @@
       <hr>
 
         <section class="comments">
-          
+          <div class="komentarze">
+          <?php $sql_select = "SELECT * FROM Komentarze ORDER BY id ASC";
+$result = $conn->query($sql_select);
+
+if ($result->num_rows > 0) {
+    echo "<h2>Dotychczasowe komentarze:</h2>";
+    while($row = $result->fetch_assoc()) {
+        echo "<strong>" . $row["login"] . "</strong>". "<br>" . $row["tresc"] . "</p>";
+    }
+} else {
+    echo "Brak dotychczasowych komentarzy.";
+}
+
+$conn->close();
+?>
+          </div>
         </section>
 
         
